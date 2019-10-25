885a8dbc69685e7dfa9133dc428b463771f13d1a
Howard Wang <howardw.wang@mail.utoronto.ca>
2019-10-25 19:17:38 -0400

    created new game manager for rock paper scissors

---
+++ phase1/game/app/src/main/java/com/example/game/RockPaperScissorsGame.java
@@ -0,0 +1,28 @@
+package com.example.game;
+
+public class RockPaperScissorsGame extends Game {
+
+    //Game manager for running and the rock paper scissors game
+
+
+
+
+    @Override
+    void display() {
+
+    }
+
+    @Override
+    void receiveInput(int x, int y) {
+    }
+
+    @Override
+    void updateDifficulty() {
+
+    }
+
+    @Override
+    int endGame() {
+        return 0;
+    }
+}
