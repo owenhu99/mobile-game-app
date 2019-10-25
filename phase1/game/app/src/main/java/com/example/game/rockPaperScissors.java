0ae9f4b1edc301102cb5f274acc08ccc75340d70
Howard Wang <howardw.wang@mail.utoronto.ca>
2019-10-25 18:54:38 -0400

    created and completed rockPaperScissors class, representing a round

---
+++ phase1/game/app/src/main/java/com/example/game/rockPaperScissors.java
@@ -0,0 +1,87 @@
+package com.example.game;
+
+
+import java.util.Random;
+
+public class rockPaperScissors extends Game{
+    private String[] choices = {"Rock", "Paper", "Scissors"};
+
+    private String currentBotChoice;
+    private String currentUserChoice;
+
+    public rockPaperScissors(String userChoice){
+        this.currentUserChoice = userChoice;
+    }
+
+    // sets the user's choice for this round
+    public void set_user_choice(String choice){
+        this.currentUserChoice = choice;
+    }
+
+
+    // The bot chooses string in array, choices. Returns that choice.
+    private void get_bot_choice() {
+        Random rand = new Random();
+        int indx = rand.nextInt(3);
+        this.currentBotChoice = choices[indx];
+    }
+
+    // returns who wins that round and tie if there's a tie
+    private String get_results() {
+        String result = "If you see this, there's an error in get_result in RPS";
+
+        switch (this.currentUserChoice) {
+            case "Rock":
+                if (this.currentBotChoice.equals("Paper")) {
+                    result = "bot";
+                } else if (this.currentBotChoice.equals("Scissors")) {
+                    result = "user";
+                } else {
+                    result = "tie";
+                }
+                break;
+            case "Paper":
+                if (this.currentBotChoice.equals("Rock")) {
+                    result = "user";
+                } else if (this.currentBotChoice.equals("Scissors")) {
+                    result = "bot";
+                } else {
+                    result = "tie";
+                }
+                break;
+
+            case "Scissors":
+                if (this.currentBotChoice.equals("Paper")) {
+                    result = "user";
+                } else if (this.currentBotChoice.equals("Rock")) {
+                    result = "bot";
+                } else {
+                    result = "tie";
+                }
+                break;
+        }
+
+        return result;
+    }
+
+    @Override
+    void display() {
+
+    }
+
+    @Override
+    void updateDifficulty() {
+
+    }
+
+    @Override
+    void receiveInput(int x, int y) {
+
+    }
+
+    @Override
+    int endGame() {
+        return 0;
+    }
+
+}
