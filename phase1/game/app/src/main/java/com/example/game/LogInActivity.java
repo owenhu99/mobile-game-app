329b906469d0c5d3199c8d670de70250e96796e5
owenhu <owenhu99@gmail.com>
2019-10-25 20:05:56 -0400

    Modified login page and signup page for user data storage and query.

--- phase1/game/app/src/main/java/com/example/game/LogInActivity.java
+++ phase1/game/app/src/main/java/com/example/game/LogInActivity.java
@@ -2,7 +2,17 @@ package com.example.game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -10,5 +20,40 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        fillUsers();
+    }
+
+    private void fillUsers() {
+        LinearLayout linearLayout = (LinearLayout)findViewById(R.id.users_menu);
+        ArrayList<User> users = getUsers();
+        if (users.size() == 0) {
+            return;
+        }
+        TextView user1 = new TextView(this);
+        user1.setText(users.get(0).getUserName());
+        user1.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        linearLayout.addView(user1);
+    }
+
+    protected ArrayList<User> getUsers() {
+        Object user;
+        ArrayList<User> users = new ArrayList<User>();
+        String fileName = getResources().getString(R.string.savefile);
+
+        try {
+            FileInputStream fis = openFileInput(fileName);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            try {
+                while ((user = ois.readObject()) != null) {
+                    users.add((User) user);
+                }
+            } finally {
+                ois.close();
+                fis.close();
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 }
--- phase1/game/app/src/main/java/com/example/game/SignUpActivity.java
+++ phase1/game/app/src/main/java/com/example/game/SignUpActivity.java
@@ -2,10 +2,15 @@ package com.example.game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -17,5 +22,22 @@ public class SignUpActivity extends AppCompatActivity {
 
     public void submit(View view) {
         Intent intent = new Intent(this, LogInActivity.class);
+        EditText firstName = (EditText) findViewById(R.id.editText1);
+        EditText lastName = (EditText) findViewById(R.id.editText2);
+        EditText userName = (EditText) findViewById(R.id.editText3);
+        User user = new User(userName.getText().toString(), firstName.getText().toString(), lastName.getText().toString());
+
+        String fileName = getResources().getString(R.string.savefile);
+        try {
+            FileOutputStream fos = openFileOutput(fileName, Context.MODE_PRIVATE);
+            ObjectOutputStream os = new ObjectOutputStream(fos);
+            os.writeObject(user);
+            os.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        startActivity(intent);
     }
 }
