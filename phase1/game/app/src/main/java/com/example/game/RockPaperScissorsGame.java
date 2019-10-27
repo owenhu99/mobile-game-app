89e06f9f6849d577725aac7258a9ce16f1e8031b
ChrisC <christopherlong.chen@mail.utoronto.ca>
2019-10-27 18:38:24 -0400

    RockPaperScissorsGame - fixed receiveInput so buttons now do something, the "round" being displayed is currently just a test

--- phase1/game/app/src/main/java/com/example/game/RockPaperScissorsGame.java
+++ phase1/game/app/src/main/java/com/example/game/RockPaperScissorsGame.java
@@ -17,18 +17,22 @@ public class RockPaperScissorsGame extends Game {
     private String roundResult;
 
     //Rectangles of buttons
-    private Rect rockBtn = new Rect();
-    private Rect paperBtn = new Rect();
-    private Rect scissorsBtn = new Rect();
+    //private Rect rockBtn = new Rect();
+    //private Rect paperBtn = new Rect();
+    //private Rect scissorsBtn = new Rect();
 
     //color of button and text
     private Paint btnColor = new Paint();
     private Paint textColor = new Paint();
+    private Paint uiTextColor = new Paint();
 
+
+    private int roundNum;
 
     RockPaperScissorsGame(int d) {
         super(d);
         createButtons();
+        roundNum = 1;
     }
 
     public static void main(Object user) {
@@ -46,17 +50,21 @@ public class RockPaperScissorsGame extends Game {
         int btnHeight = height / 10;
         int btnWidth = width / 5;
 
+        /*
         rockBtn.set(10, height / 2,
                 10 + btnWidth, height / 2 + btnHeight);
         paperBtn.set((width / 2 - btnWidth / 2), height / 2,
                 width / 2 + btnWidth / 2, height / 2 + btnHeight);
         scissorsBtn.set((width - 10 - btnWidth), height / 2,
-                width - 10, height / 2 + btnHeight);
+                width - 10, height / 2 + btnHeight);*/
 
         btnColor.setColor(Color.GRAY);
         textColor.setColor(Color.RED);
         textColor.setTextSize(50);
         textColor.setTextAlign(Paint.Align.CENTER);
+
+        uiTextColor.setColor(Color.WHITE);
+        uiTextColor.setTextSize(50);
 
     }
 
@@ -71,21 +79,21 @@ public class RockPaperScissorsGame extends Game {
         int btnHeight = height / 12;
         int btnWidth = width / 5;
 
-        canvas.drawRect(10, height / 2,
-                10 + btnWidth, height / 2 + btnHeight, btnColor);
+        canvas.drawRect(20, height / 2,
+                20 + btnWidth, height / 2 + btnHeight, btnColor);
         canvas.drawRect((width / 2) - (btnWidth / 2), height / 2,
                 (width / 2) + (btnWidth / 2), height / 2 + btnHeight, btnColor);
-        canvas.drawRect(width - 10 - btnWidth, height / 2,
-                width - 10, height / 2 + btnHeight, btnColor);
+        canvas.drawRect(width - 20 - btnWidth, height / 2,
+                width - 20, height / 2 + btnHeight, btnColor);
 
-        canvas.drawText("Rock", 10 + (btnWidth / 2), height / 2 + (btnHeight / 2)
+        canvas.drawText("Rock", 20 + (btnWidth / 2), height / 2 + (btnHeight / 2)
                 + textColor.getTextSize() / 2, textColor);
         canvas.drawText("Paper", (width / 2), height / 2 + (btnHeight / 2)
                 + textColor.getTextSize() / 2, textColor);
-        canvas.drawText("Scissors", width - 10 - (btnWidth / 2), height / 2 +
+        canvas.drawText("Scissors", width - 20 - (btnWidth / 2), height / 2 +
                 (btnHeight / 2) + textColor.getTextSize() / 2, textColor);
 
-
+        canvas.drawText("Round: " + roundNum, 10, height / 3, uiTextColor);
     }
 
     @Override
@@ -96,20 +104,29 @@ public class RockPaperScissorsGame extends Game {
 
     @Override
     void receiveInput(int x, int y) {
+        int btnHeight = height / 12;
+        int btnWidth = width / 5;
+
         RockPaperScissors currRound;
 
-        if ((x >= rockBtn.left && x <= rockBtn.right) &&
-                (y >= rockBtn.top && y <= rockBtn.bottom)) {
+        if ((x >= 20 && x <= 20 + btnWidth) &&
+                (y >= height / 2 && y <= height / 2 + btnHeight)) {
+            roundNum++;
+
             currRound = new RockPaperScissors("Rock");
             currRound.setBotChoice();
             roundResult = currRound.findResult();
-        } else if ((x >= paperBtn.left && x <= paperBtn.right) &&
-                (y >= paperBtn.top && y <= paperBtn.bottom)) {
+        } else if ((x >= (width / 2) - (btnWidth / 2) && x <= (width / 2) + (btnWidth / 2)) &&
+                (y >= height / 2 && y <= height / 2 + btnHeight)) {
+            roundNum--;
+
             currRound = new RockPaperScissors("Paper");
             currRound.setBotChoice();
             roundResult = currRound.findResult();
-        } else if ((x >= scissorsBtn.left && x <= scissorsBtn.right) &&
-                (y >= scissorsBtn.top && y <= scissorsBtn.bottom)) {
+        } else if ((x >= width - 20 - btnWidth && x <= width - 20) &&
+                (y >= height / 2 && y <= height / 2 + btnHeight)) {
+            roundNum = roundNum * 2;
+
             currRound = new RockPaperScissors("Scissors");
             currRound.setBotChoice();
             roundResult = currRound.findResult();
@@ -118,6 +135,9 @@ public class RockPaperScissorsGame extends Game {
 
     @Override
     int endGame() {
+        gameEnded = true;
+
+
         return 0;
     }
 
