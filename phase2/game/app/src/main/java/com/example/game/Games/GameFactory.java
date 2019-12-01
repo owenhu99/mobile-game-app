560a8bb0458e9466e6af21d1221cef2305e33afc
ChrisC <christopherlong.chen@mail.utoronto.ca>
2019-12-01 17:29:45 -0500

    Changed memory game to follow facade pattern. MemoryFacde extends game and delegates tasks to MemoryGame and MemoryDrawer; moved appropriate code from old MemoryGame class around into new classes

--- phase2/game/app/src/main/java/com/example/game/Games/GameFactory.java
+++ phase2/game/app/src/main/java/com/example/game/Games/GameFactory.java
@@ -1,5 +1,6 @@
 package com.example.game.Games;
 
+import com.example.game.Games.MemoryGame.MemoryFacade;
 import com.example.game.Games.MemoryGame.MemoryGame;
 import com.example.game.Games.RoomEscapeGame.RoomEscape;
 import com.example.game.Games.RunnerGame.RunnerGame;
@@ -12,7 +13,7 @@ public class GameFactory {
         } else if (name == "Runner"){
             return new RunnerGame(width, height);
         } else if (name == "Memory"){
-            return new MemoryGame(width, height);
+            return new MemoryFacade(width, height);
         }
         return null;
     }
